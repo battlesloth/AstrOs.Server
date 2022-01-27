21b19fc588811b3e7a3a0258efda2709da75d9dd
Jeff Rector <jrector614@gmail.com>
2022-01-27 08:19:42 -0500

    #1 - working on websocket

--- client/src/app/services/websocket/websocket.service.ts
+++ client/src/app/services/websocket/websocket.service.ts
@@ -6,5 +6,33 @@ import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 })
 export class WebsocketService {
 
-  constructor() { }
+  webSocket: WebSocketSubject<unknown>;
+
+  subscriptions: Map<string, SocketSubscription>
+
+  constructor() {
+    this.webSocket = webSocket('ws://' + window.location.hostname + ':5000');
+
+    this.webSocket.subscribe({
+      next: (msg) => this.processMessage(msg),
+      error: (err) => console.log(err),
+      complete: () => console.log('socket disconnected')
+    });
+  }
+
+  processMessage(message: any) {
+    if (message['module']) {
+      switch (message['module']) {
+        case 'core':
+          this.coreUp = this.handleStatus(message['status'], this.coreEl, this.coreUp);
+          break;
+        case 'dome':
+          break;
+        case 'body':
+          break;
+        case 'legs':
+          break;
+      }
+    }
+  }
 }
